58726f765a6ef4d4e7c81462b2933a36ca199b21
Majid G.E <mgetech@yahoo.com>
2025-05-17 21:09:33 +0200

    Swagger UI added, deploy user service and deploy order service

--- app/microservices/order/src/main/java/com/example/order/controller/OrderController.java
+++ app/microservices/order/src/main/java/com/example/order/controller/OrderController.java
@@ -4,6 +4,8 @@ import com.example.order.dto.OrderRequestDTO;
 import com.example.order.dto.OrderResponseDTO;
 import com.example.order.entity.OrderStatus;
 import com.example.order.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
@@ -12,20 +14,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
+@Tag(name = "Order API", description = "Handles order creation, retrieval, and status updates")
 public class OrderController {
 
     private final OrderService orderService;
 
+    @Operation(summary = "Create a new order", description = "Creates an order by getting userID and productID validation from User and Product services.")
     @PostMapping
     public ResponseEntity<OrderResponseDTO> create(@RequestBody @Valid OrderRequestDTO request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(request));
     }
 
+    @Operation(summary = "Get order by ID", description = "Fetches order details by order ID")
     @GetMapping("/{id}")
     public OrderResponseDTO get(@PathVariable Long id) {
         return orderService.getOrder(id);
     }
 
+    @Operation(summary = "Update order status", description = "Changes the status of an existing order")
     @PatchMapping("/{id}/status")
     public ResponseEntity<OrderResponseDTO> updateStatus(
             @PathVariable Long id,
